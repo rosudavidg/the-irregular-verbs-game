eb06c32983d9c2d8caafb20a7c4a813f0e8f9971
rosudavidg <rosudavidg@gmail.com>
2020-11-23 21:26:49 +0200

    added python backend

---
+++ backend/src/database.py
@@ -0,0 +1,77 @@
+import os
+from psycopg2 import connect
+from get_docker_secret import get_docker_secret
+from random import randint
+
+# Set environment values
+database_name = get_docker_secret(os.environ['DATABASE_DB'])
+database_user = get_docker_secret(os.environ['DATABASE_USER'])
+database_password = get_docker_secret(os.environ['DATABASE_PASSWORD'])
+database_host = "database"
+database_port = "5432"
+
+
+class DatabaseConnection():
+    """
+    Abstracts the database connection
+    """
+
+    def __init__(self):
+        """
+        Init the connection
+        """
+
+        self.connection = connect(user=database_user,
+                                  password=database_password,
+                                  host=database_host,
+                                  port=database_port,
+                                  database=database_name)
+
+    def get_cursor(self):
+        """
+        Returns a cursor
+        """
+
+        return self.connection.cursor()
+
+    def close(self):
+        """
+        Close the connection
+        """
+
+        self.connection.close()
+
+
+def select_random_verb():
+    """
+    Selects a random verb from database and is returned
+    """
+
+    try:
+        db_con = DatabaseConnection()
+        cursor = db_con.get_cursor()
+    except:
+        raise Exception("Cannot connect to database.")
+
+    try:
+        # Get number of entries
+        cursor.execute("SELECT COUNT(*) FROM irregular_verbs")
+        (verbs_count,) = cursor.fetchone()
+
+        if verbs_count < 1:
+            raise Exception('There\'s no entry in the database.')
+
+        # Get random verb
+        cursor.execute(
+            f'SELECT infitive, simple_past, past_particile FROM irregular_verbs WHERE id = {randint(1, verbs_count)}')
+
+        verb = cursor.fetchone()
+
+        return {
+            'infitive': verb[0],
+            'simple_past': verb[1],
+            'past_particile': verb[2]
+        }
+
+    finally:
+        db_con.close()
---
+++ backend/src/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, Response, make_response
+from database import select_random_verb
+
+app = Flask(__name__)
+
+
+@app.route('/verbs/random', methods=['GET'])
+def get_random_verb():
+    """
+    Returns a random verb from database
+    """
+
+    try:
+        verb = select_random_verb()
+
+        return Response(str(verb), status=200, mimetype='application/json')
+    except:
+        return Response("Selecting a random verb failed!", status=500)
+
+
+if __name__ == "__main__":
+    """
+    Server entrypoint
+    """
+
+    app.run(host="0.0.0.0")
