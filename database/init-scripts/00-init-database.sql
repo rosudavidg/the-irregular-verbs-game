674f0cf80c3e47a806eef1be746a67ae077cec55
rosudavidg <rosudavidg@gmail.com>
2020-11-23 19:57:21 +0200

    fixed missing table name and renamed

---
+++ database/init-scripts/00-init-database.sql
@@ -0,0 +1,11 @@
+-- Create verbs table
+CREATE TABLE IF NOT EXISTS irregular_vebs (
+    -- Autoincrement id
+    id SERIAL PRIMARY KEY,
+    -- Infinitive
+    infitive VARCHAR (32),
+    -- Simple past(s) - list of accepted words
+    simple_past VARCHAR (128),
+    -- Past participle(s) - list of accepted words
+    past_particile VARCHAR (128)
+);
