f88abfed6ffb814f51d942bd2c753f9d12961198
rosudavidg <rosudavidg@gmail.com>
2020-11-23 21:59:27 +0200

    added base for frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './Header'
+import Game from './Game'
+import Footer from './Footer'
 
 function App() {
-  return <div>Hello</div>
+  return <div className="app">
+    <Header />
+    <Game />
+    <Footer />
+  </div>
 }
 
 export default App;
---
+++ frontend/src/components/Game.js
@@ -0,0 +1,8 @@
+import './Game.css';
+
+function Game() {
+    console.log(`${process.env.REACT_APP_API_URL}`)
+    return <div>Game</div>
+}
+
+export default Game;
