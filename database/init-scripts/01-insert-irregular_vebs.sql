b9c0ddcfef40496d5128c1e60f938087292addbd
rosudavidg <rosudavidg@gmail.com>
2020-11-23 20:02:23 +0200

    init irregular verbs table

---
+++ database/init-scripts/01-insert-irregular_vebs.sql
@@ -0,0 +1,3 @@
+INSERT INTO irregular_verbs (infitive, simple_past, past_particile) VALUES ('be', 'was,were', 'been');
+INSERT INTO irregular_verbs (infitive, simple_past, past_particile) VALUES ('go', 'went', 'gone');
+INSERT INTO irregular_verbs (infitive, simple_past, past_particile) VALUES ('beat', 'beat', 'beaten,beat');
