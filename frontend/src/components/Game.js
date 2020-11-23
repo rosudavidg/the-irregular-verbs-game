cc20d5691bdf4c302a93050d29a4fd5d2928060d
rosudavidg <rosudavidg@gmail.com>
2020-11-23 23:03:15 +0200

    fixed backend proxy

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,8 +1,24 @@
 import './Game.css';
+import axios from 'axios';
+import { useState, useEffect } from "react"
+
+const getIrregularVerb = async (setIrregularVerb) => {
+    axios.get(`${process.env.REACT_APP_API_URL}/verbs/random`, {}).then((res) => {
+        // setIrregularVerb(res.data)
+        console.log(res.data)
+    }).catch(() => {
+        alert('Getting a new irregular verb failed.');
+    });
+}
 
 function Game() {
-    console.log(`${process.env.REACT_APP_API_URL}`)
-    return <div>Game</div>
+    const [irregularVerb, setIrregularVerb] = useState('halkalla')
+
+    useEffect(() => {
+        getIrregularVerb(setIrregularVerb);
+    }, [])
+
+    return <div>{irregularVerb}</div>
 }
 
 export default Game;
