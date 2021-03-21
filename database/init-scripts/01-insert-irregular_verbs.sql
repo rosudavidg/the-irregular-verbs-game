1aa4c3bebe2396a2f19ab950c51164f1d8956cfb
rosudavidg <rosudavidg@gmail.com>
2021-03-21 17:33:31 +0200

    Added more verbs

--- database/init-scripts/01-insert-irregular_verbs.sql
+++ database/init-scripts/01-insert-irregular_verbs.sql
@@ -1,4 +1,38 @@
 -- Insert irregular verbs into database
 INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('be', 'was,were', 'been');
-INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('go', 'went', 'gone');
+
 INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('beat', 'beat', 'beaten,beat');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('become', 'became', 'become');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('begin', 'began', 'begun');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('break', 'broke', 'broken');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('bring', 'brought', 'brought');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('build', 'built', 'built');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('buy', 'bought', 'bought');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('catch', 'caught', 'caught');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('choose', 'chose', 'chosen');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('come', 'came', 'come');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('cost', 'cost', 'cost');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('cut', 'cut', 'cut');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('do', 'did', 'done');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('draw', 'drew', 'drawn');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('dream', 'dreamt,dreamed', 'dreamt,dreamed');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('drink', 'drank', 'drunk');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('drive', 'drove', 'driven');
+
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('eat', 'ate', 'eaten');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('fall', 'fell', 'fallen');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('feed', 'fed', 'fed');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('feel', 'felt', 'felt');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('fight', 'fought', 'fought');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('find', 'found', 'found');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('fly', 'flew', 'flown');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('forget', 'forgot', 'forgotten');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('get', 'got', 'got,gotten');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('give', 'gave', 'given');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('go', 'went', 'gone');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('hang', 'hung', 'hung');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('have', 'had', 'had');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('hear', 'heard', 'heard');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('hide', 'hid', 'hidden');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('hit', 'hit', 'hit');
+INSERT INTO irregular_verbs (infinitive, simple_past, past_participle) VALUES ('hold', 'held', 'held');
