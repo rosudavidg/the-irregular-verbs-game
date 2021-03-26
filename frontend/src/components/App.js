bdfcd46d094e57f587de9e24342f213fc113073e
rosudavidg <rosudavidg@gmail.com>
2021-03-27 00:34:52 +0200

    Added helmet title

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Helmet } from 'react-helmet'
 import Header from './Header'
 import Game from './Game'
 import Footer from './Footer'
 
 function App() {
+  const TITLE = "The Irregular Verbs Game";
+
   return <div className="app">
+    <Helmet>
+      <title>{TITLE}</title>
+    </Helmet>
     <Header />
     <Game />
     <Footer />
